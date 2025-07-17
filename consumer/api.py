26d9796c30a6feb7a7d9315bbb77c09dc7bc18bc
Devansh <devanshdhaka007@gmail.com>
2025-07-17 12:42:25 +0530

    feat: enhance live GPS tracking API to include full route information

--- consumer/api.py
+++ consumer/api.py
@@ -1,23 +1,27 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from flask import Flask, jsonify, render_template
 from threading import Thread
 from gps_consumer import start_consumer, latest_positions
+from producer.fetch_route import route_points  # ✅ now works
+
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/live")
 def live_positions():
-    """Return latest GPS positions of all cars"""
-    return jsonify(latest_positions)
+    """Return latest GPS positions + full route"""
+    return jsonify({
+        "positions": latest_positions,
+        "route": route_points  # full route coordinates
+    })
 
 @app.route("/")
 def map_view():
-    """Show live map"""
     return render_template("map.html")
 
 if __name__ == "__main__":
-    # Run Kafka consumer in a separate thread
     t = Thread(target=start_consumer, daemon=True)
     t.start()
-
-    # Start Flask API on port 5001
     app.run(host="0.0.0.0", port=5001)
